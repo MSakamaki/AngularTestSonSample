380b411d349b386b1db4828c46344eb915dc5836
Mitsuru Ogawa <mitsuru.ogawa.jp@gmail.com>
2015-01-27 22:20:16 +0900

    [WIP]Add some feature

--- client/app/cart/cart.controller.js
+++ client/app/cart/cart.controller.js
@@ -1,6 +1,20 @@
 'use strict';
 
 angular.module('AngularJsTestson')
-  .controller('CartCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('CartCtrl', function ($scope, cartItem) {
+
+    $scope.cartItems = cartItem.items;
+
+    $scope.addCart = function(index) {
+      cartItem.add($scope.cartItems[index]);
+    };
+
+    $scope.removeCart = function(index) {
+      cartItem.remove($scope.cartItems[index]);
+    };
+
+    $scope.clearCart = function() {
+      cartItem.clear();
+    }
+
   });
