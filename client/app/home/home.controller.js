4aae4f22b4c308fe557626e8be97bb95ecab572c
Mitsuru Ogawa <mitsuru.ogawa.jp@gmail.com>
2015-01-27 22:09:59 +0900

    [WIP]Add some feature

--- client/app/home/home.controller.js
+++ client/app/home/home.controller.js
@@ -1,6 +1,17 @@
 'use strict';
 
 angular.module('AngularJsTestson')
-  .controller('HomeController', [function(){
-    this.message = 'Hello AngularJsTestson home';
-  }]);
+  .controller('HomeController', function($scope, $http, cartItem){
+
+    $scope.message = 'Hello AngularJsTestson home';
+    $scope.cartItems = [];
+
+    $http.get('/api/products').success(function(data) {
+      $scope.products = data;
+    });
+
+    $scope.addCart = function(index) {
+      cartItem.add($scope.products[index]);
+    };
+
+  });
